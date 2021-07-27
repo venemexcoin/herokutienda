bbaf2b167b79e9e05e47b751b88d8b9cb0c78c83
venemexcoin <venemexcoin@gmail.com>
2021-07-27 12:54:53 -0500

    commit Add increase & decrease in cart

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -172,7 +172,10 @@
 								<a href="#" class="link-direction">
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
-										<span class="index">4 items</span>
+										{{-- cantidad de articulos en el carro --}}
+										@if (Cart::count() > 0)
+										<span class="index">{{Cart::count()}} items</span>
+										@endif</>
 										<span class="title">CART</span>
 									</div>
 								</a>
