f576ed30bdf4c0d9cbd6b852fd4563a1b4ded7da
venemexcoin <venemexcoin@gmail.com>
2021-08-11 21:21:15 -0500

    commit Add slider Home menu

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -90,6 +90,9 @@
 														<a title="Categories" href="{{route('admin.products')}}">All Products</a>
 													</li>
 													<li class="menu-item" >
+														<a title="Categories" href="{{route('admin.homeslider')}}">Manage Home slider</a>
+													</li>
+													<li class="menu-item" >
 														<a title="Logout" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit(); ">Logout</a>
 													</li>
 													<form id="logout-form" method="POST" action="{{route('logout')}}">
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ use App\Http\Livewire\Admin\AdminEditCategoryComponent;
 use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\Admin\AdminEditProductComponent;
+use App\Http\Livewire\Admin\AdminHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminAddHomeSliderComponent;
+use App\Http\Livewire\Admin\AdminEditHomeSliderComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -63,4 +66,7 @@ Route::middleware(['auth:sanctum','verified','authadmin'])->group(function() {
     Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
     Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.addproduct');
     Route::get('/admin/product/edit/{product_slug}', AdminEditProductComponent::class)->name('admin.editproduct');
+    Route::get('/admin/sider', AdminHomeSliderComponent::class)->name('admin.homeslider');
+    Route::get('/admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
+    Route::get('/admin/slider/edid/{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
 });
